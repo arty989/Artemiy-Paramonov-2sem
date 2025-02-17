459875e46cf6df1506e545685997dc7ec3870278
Artemiy <artemiyyparamonov@gmail.com>
2025-02-17 14:57:23 +0300

    Добавил E2E тест

---
+++ src/test/java/com/example/S2_H1/e2e/EndToEndTest.java
@@ -0,0 +1,35 @@
+package com.example.S2_H1.e2e;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class EndToEndTest {
+
+  @LocalServerPort
+  private int port;
+
+  @Autowired
+  private TestRestTemplate restTemplate;
+
+  @Test
+  public void testGetAllAvailableSites() {
+    ResponseEntity<Map> response = restTemplate.getForEntity("http://localhost:" + port + "/sites", Map.class);
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+    Map sites = response.getBody();
+    assertNotNull(sites);
+    assertTrue(sites.containsValue("https://habr.com/ru/news/"));
+  }
+}
