9da79da265473ce77d16948071a043c9180b540e
Artemiy <artemiyyparamonov@gmail.com>
2025-03-10 20:59:21 +0300

    Поправил ещё 1 тест

--- src/test/java/com/example/S2_H1/ApplicationTests.java
+++ src/test/java/com/example/S2_H1/ApplicationTests.java
@@ -2,10 +2,12 @@ package com.example.S2_H1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@ActiveProfiles("no-db-test")
 class ApplicationTests {
 
 	@Test
